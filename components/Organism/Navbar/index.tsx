17ee852dc1f68b8565fa6ae4109c905e04b885d8
NovaSugiantara <novasugiantara425@gmail.com>
2023-03-30 11:41:03 +0800

    refactor code

--- components/Organism/Navbar/index.tsx
+++ components/Organism/Navbar/index.tsx
@@ -1,52 +1,42 @@
 import Image from "next/image";
+import React from "react";
+import Menu from "./menu";
 import Auth from "./auth";
-import Menus from "./menus";
 
-export default function Navbar() {
+export default function index() {
   return (
-      <>
-        <section>
+    <>
+      <section>
         <nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/#">
-                    <Image src="/icon/logo.svg" width={60} height={60} />
-                </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ms-auto text-lg gap-lg-0 gap-2">
-                        <Menus title="Home" active />
-                        <Menus title="Games" />
-                        <Menus title="Rewards" />
-                        <Menus title="Discover"  />
-                        <Menus title="Global Ranks" />
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/#">
+              <Image alt="logo" src="/icon/logo.svg" width={60} height={60} />
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ms-auto text-lg gap-lg-0 gap-2">
+                <Menu title="Home" active />
+                <Menu title="Games" href="/games" />
+                <Menu title="Rewards" href="/rewards" />
+                <Menu title="Discover" href="/discovers" />
+                <Menu title="Global Ranks" href="/global-ranks" />
 
-                        <Auth />
-                         <li className="nav-item my-auto dropdown d-flex">
-                            <div className="vertical-line d-lg-block d-none"></div>
-                            <div>
-                                <a className="dropdown-toggle ms-lg-40" href="#" role="button" id="dropdownMenuLink"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    <img src="/img/avatar-1.png" className="rounded-circle" width="40" height="40"
-                                        alt="" />
-                                </a>
-
-                                <ul className="dropdown-menu border-0" aria-labelledby="dropdownMenuLink">
-                                    <li><a className="dropdown-item text-lg color-palette-2" href="#">My Profile</a></li>
-                                    <li><a className="dropdown-item text-lg color-palette-2" href="#">Wallet</a></li>
-                                    <li><a className="dropdown-item text-lg color-palette-2" href="#">Account Settings</a>
-                                    </li>
-                                    <li><a className="dropdown-item text-lg color-palette-2" href="#">Log Out</a></li>
-                                </ul>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+                <Auth />
+              </ul>
             </div>
+          </div>
         </nav>
-    </section>
-      < />
+      </section>
+    </>
   );
 }
